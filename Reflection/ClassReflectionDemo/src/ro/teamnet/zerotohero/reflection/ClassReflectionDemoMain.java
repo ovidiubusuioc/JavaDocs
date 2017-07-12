027aa6b2ea53726bf04ac59dc093bc9bd8ce1554
ovidiubusuioc <ovidiu.busuioc@ymail.com>
2017-07-12 09:39:49 +0300

    Add files via upload

---
+++ Reflection/ClassReflectionDemo/src/ro/teamnet/zerotohero/reflection/ClassReflectionDemoMain.java
@@ -0,0 +1,65 @@
+package ro.teamnet.zerotohero.reflection;
+
+/**
+ * TODO
+ * in order to resolve the exercises below, you will have to create
+ * all the needed clasees, with their members and methods
+ * (in ro.teamnet.zerotohero.reflection.demoobjects package)
+ */
+public class ClassReflectionDemoMain {
+    public static void main(String[] args) {
+        //TODO get the class for a String object, and print it
+		
+
+        //TODO get the class of an Enum, and print it
+        
+
+        //TODO get the class of a collection, and print it
+        
+
+        //TODO get the class of a primitive type, and print it
+        
+
+        //TODO get and print the class for a field of primitive type
+        
+
+        //TODO get and print the class for a primitive type, using the wrapper class
+        
+
+        //TODO get the class for a specified class name
+        
+
+        //TODO get the superclass of a class, and print it
+        //TODO get the superclass of the superclass above, and print it
+        
+
+        //TODO get and print the declared classes within some other class
+        
+
+        //TODO print the number of constructors of a class
+        
+
+        //TODO get and invoke a public constructor of a class
+        
+
+        //TODO get and print the class of one private field 
+        
+		
+		//TODO set and print the value of one private field for an object
+        
+
+        //TODO get and print only the public fields class
+        
+
+        //TODO get and invoke one public method of a class
+        
+
+        //TODO get and invoke one inherited method of a class
+       
+		
+		//TODO invoke a method of a class the classic way for ten times, and print the timestamp (System.currentTimeMillis())
+		//TODO invoke a method of a class by Reflection for 100 times, and print the timestamp
+		//what do you observe?
+		
+    }
+}
